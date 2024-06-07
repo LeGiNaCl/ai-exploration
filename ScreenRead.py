d21d16711246924b90f0f735aae85ed1a09d257f
LeGiNaCl <mathieu.gagnon9@outlook.com>
2024-06-06 20:38:59 -0400

    Cleaned screen read

--- ScreenRead.py
+++ ScreenRead.py
@@ -9,7 +9,33 @@ class ScreenRead:
     def __init(self):
         print("Screen Handler")
     
-    def get_framebuffer(self,handle):
+    def process_image(self, handle):
+        result, image = self.get_image(handle)
+        minimap = self.extract_minimap(image)
+        grayscale_img = self.grayscale_conversion(image)
+        filtered_imgs = self.apply_filters(grayscale_img, [
+            ImageFilter.CONTOUR,
+            ImageFilter.EDGE_ENHANCE,
+            ImageFilter.EDGE_ENHANCE_MORE,
+            ImageFilter.EMBOSS,
+            ImageFilter.EMBOSS
+        ])
+        i = 0
+        for img in filtered_imgs:
+            img.save(f"test{i}.png")
+            i+=1
+        return result, image
+    
+    def grayscale_conversion(self, image: Image):
+        return image.convert("L")
+    
+    def apply_filters(self, image:Image, filters: list)->list:
+        filtered_img = []
+        for filter in filters:
+            filtered_img.append(image.filter(filter))
+        return filtered_img
+    
+    def get_image(self, handle):
         left, top, right, bot = wgui.GetWindowRect(handle)
         w = right - left
         h = bot - top
@@ -36,56 +62,14 @@ class ScreenRead:
         mfcDC.DeleteDC()
         wgui.ReleaseDC(handle, handleDC)
         im.save ("Test.png")
-        imconv1 = im.convert("L")
-        
-        imfil1 = imconv1.filter(ImageFilter.CONTOUR)
-        imfil2 = imconv1.filter(ImageFilter.EDGE_ENHANCE)
-        imfil3 = imconv1.filter(ImageFilter.EDGE_ENHANCE_MORE)
-        imfil4 = imconv1.filter(ImageFilter.EMBOSS)
-        imfil5 = imconv1.filter(ImageFilter.EMBOSS)
-        
-        imfil1.save ("testfilter1.png")
-        imfil2.save ("testfilter2.png")
-        imfil3.save ("testfilter3.png")
-        imfil4.save ("testfilter4.png")
-        imfil5.save ("testfilter5.png")
-        imconv1.save ("testcon1.png")
-        
+          
         return result, im
     
-    def get_minimap (self,handle):
-        left, top, right, bot = wgui.GetWindowRect(handle)
-        w = right - left
-        h = bot - top
-        handleDC = wgui.GetWindowDC(handle)
-        mfcDC = wui.CreateDCFromHandle(handleDC)
-        saveDC = mfcDC.CreateCompatibleDC()
-        
-        saveBitMap = wui.CreateBitmap()
-        saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
-            
-        saveDC.SelectObject(saveBitMap)
-            
-        result = windll.user32.PrintWindow(handle, saveDC.GetSafeHdc(), 0)
-        bmpinfo = saveBitMap.GetInfo()
-        bmpstr = saveBitMap.GetBitmapBits(True)
-
-        im = Image.frombuffer(
-                'RGB',
-                (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-                bmpstr, 'raw', 'BGRX', 0, 1)
-
-        wgui.DeleteObject(saveBitMap.GetHandle())
-        saveDC.DeleteDC()
-        mfcDC.DeleteDC()
-        wgui.ReleaseDC(handle, handleDC)
-            
+    def extract_minimap(self, image):
         cleft= 812
         cright= 912
         ctop= 67
         cbottom= 154
-        minimap = im.crop((cleft,ctop,cright,cbottom))
-        
-        minimap.save ("minimap.png")
-        
-        return result, minimap
+        minimap = image.crop((cleft,ctop,cright,cbottom))
+                
+        return minimap
